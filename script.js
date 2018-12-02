d5e34697f8f139dfec312e083b2df5bec0a15b67
kengon1315 <45412598+kengon1315@users.noreply.github.com>
2018-12-02 00:01:09 -0700

    Derek Changes
    Changes Derek Wanted

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-$(document).ready(function(){
-	$(".menu_option").hover(
-		function(){
-			$(".dropMenu").hide();
-			var id = "#" + $(this).attr("id") + "Menu";
-			$(id).show();
-			$(id).hover(
-				function(){},
-				function(){
-					$(this).hide();
-				});
-		});
-		
+$(document).ready(function(){	
 	$(".Order").hover(
 		function(){
 			
